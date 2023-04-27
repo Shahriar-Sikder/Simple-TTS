01ea828506880fd79f4643e29caad99158b62e0d
Shahriar-Sikder <70928274+Shahriar-Sikder@users.noreply.github.com>
2023-04-27 13:27:06 +0600

    Add files via upload

---
+++ Simple TTS/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace Simple_TTS
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speech;
+        public Form1()
+        {
+            InitializeComponent();
+            speech = new SpeechSynthesizer();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > 0)
+            {
+                speech.SpeakAsync(textBox1.Text);
+            }
+        }
+    }
+}
